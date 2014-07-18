ed4a8c2fadb6bb20365f78b166c43d05bbfa51dc
staltz <andrestaltz@gmail.com>
2014-07-18 22:47:04 +0300

    Improved build task and references to src modules

--- gulpfile.js
+++ gulpfile.js
@@ -11,44 +11,49 @@ gulp.task('coffee', function() {
   gutil.log(gutil.colors.yellow("Running CoffeeScript compiler..."));
   return gulp.src(['./src/**/*.coffee'])
     .pipe(coffee({bare: true}).on('error', gutil.log))
-    .pipe(gulp.dest('./.tmp/src/js'));
+    .pipe(gulp.dest('./build/src'));
 });
 
 gulp.task('browserify', ['coffee'], function() {
   gutil.log(gutil.colors.yellow("Packing with browserify..."));
-  return gulp.src(['./.tmp/src/js/**/*.js'])
+  return gulp.src(['./build/src/**/*.js'])
     .pipe(browserify({
       insertGlobals: true,
       debug: true
     }))
-    .pipe(gulp.dest('./.tmp/browserified-js'));
+    .pipe(gulp.dest('./build/browserified-js'));
 });
 
 gulp.task('uglify', ['browserify'], function() {
   gutil.log(gutil.colors.yellow("Minifying with uglify..."));
-  return gulp.src(['./.tmp/browserified-js/*.js'])
+  return gulp.src(['./build/browserified-js/*.js'])
     .pipe(uglify())
     .pipe(gulp.dest('./js'));
 })
 
 gulp.task('post-clean-up', ['uglify'], function() {
   gutil.log(gutil.colors.yellow("Cleaning up temporary files..."));
-  return gulp.src('./.tmp', { read: false })
+  return gulp.src(['./build/src/**/*', './build/browserified-js'], { read: false })
     .pipe(rimraf());
 });
 
 gulp.task('build', ['post-clean-up']);
 
+gulp.task('dev-build', ['browserify'], function() {
+  return gulp.src(['./build/browserified-js/*.js'])
+    .pipe(gulp.dest('./js'))
+});
+
 gulp.task('default', function() {
-  gulp.run('build');
+  gulp.run('dev-build');
 
   gutil.log("Watching for changes...");
   gulp.watch(['./src/**/*.coffee'], function(event) {
     if (event.path) {
       gutil.log("Change detected in ", gutil.colors.magenta(event.path));
     } else {
-      gutil.log("Change in source file detected. Recoffeeifying...");
+      gutil.log("Change in source file detected.");
     }
-    gulp.run('coffeeify');
+    gulp.run('dev-build');
   });
 });
