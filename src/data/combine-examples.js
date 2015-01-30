656b75707947f414675b4d58bd7dc50935b7368e
Andre Medeiros <andre.medeiros@futurice.com>
2015-01-30 19:53:19 +0200

    Add withLatestFrom example

--- src/data/combine-examples.js
+++ src/data/combine-examples.js
@@ -57,6 +57,19 @@ module.exports = {
     }
   },
 
+  "withLatestFrom": {
+    "label": "withLatestFrom((x, y) => \"\" + x + y)",
+    "inputs": [
+      [{t:0, d:1}, {t:20, d:2}, {t:65, d:3}, {t:75, d:4}, {t:92, d:5}],
+      [{t:10, d:"A"}, {t:25, d:"B"}, {t:50, d:"C"}, {t:57, d:"D"}]
+    ],
+    "apply": function(inputs) {
+      return inputs[0].withLatestFrom(inputs[1],
+        (x, y) => ("" + x.get('content') + y.get('content'))
+      );
+    }
+  },
+
   "zip": {
     "label": "zip",
     "inputs": [
