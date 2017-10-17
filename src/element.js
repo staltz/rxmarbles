c7be8da7a60397e6b3457bde94bc7d6d8b7043db
Christian Johns <xtianjohns@connate.tech>
2017-10-16 17:41:27 -0700

    Resolve empty emission bug
    Fix issue where sandbox skips first emission from sources. Initialize
    store with stream that emits values in both sources/sinks to get a
    double emission.

--- src/element.js
+++ src/element.js
@@ -7,18 +7,19 @@ import { merge } from 'ramda';
 class SandboxElement extends HTMLElement {
   connectedCallback() {
     let key = this.attributes.key.value;
+    const initialStore = { route: key, inputs: undefined };
     function main( sources ) {
       const sandbox = Sandbox( sources );
       const sinks = {
         DOM: sandbox.DOM,
-        store: sandbox.data.scan(merge, { route: key, inputs: undefined }),
+        store: sandbox.data.scan(merge, initialStore).startWith( initialStore ),
       };
       return sinks;
     }
 
     run(main, {
       DOM: makeDOMDriver(this),
-      store: sources => sources,
+      store: sources => sources.startWith( initialStore ),
     });
   }
 }
